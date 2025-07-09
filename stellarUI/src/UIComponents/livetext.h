ebd3084674f9960aab8286391bed6b2b079747da
xxxxx000000 <Allen2008yan@outlook.com>
2025-07-09 17:38:03 +0800

    Add files via upload

---
+++ stellarUI/src/UIComponents/livetext.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "component.h"
+#include <functional>
+
+/*
+    适用于动态的文本，参数为横纵坐标，颜色，lambda表达式，字体大小
+    表达式内为要显示的文本
+    Suitable for dynamic text display, with parameters: X and Y coordinates, color, lambda expression, and font size
+    The lambda expression provides the text to be displayed
+
+*/
+
+class LiveText : public Component {
+private:
+    std::function<String()> textProvider;
+    String lastRenderedText;
+    int textSize;
+    int anchorX, anchorY;
+
+public:
+    LiveText(int _x, int _y, uint16_t _color, std::function<String()> _provider, int _textSize = 2)
+        : Component(_x, _y, _color), textProvider(_provider), textSize(_textSize) {}
+
+    void draw(TFT_eSprite& buf, int offsetX = 0, int offsetY = 0) override {
+        if (!enabled) return;
+
+        String newText = textProvider ? textProvider() : "";
+
+        lastRenderedText = newText;
+
+        buf.setTextColor(color);
+        buf.setTextSize(textSize);
+        buf.drawString(newText, x + offsetX, y + offsetY);
+    }
+
+    void update(uint32_t now) override {
+        Component::update(now);
+    }
+
+    bool containsPoint(int px, int py) override {
+        return false;
+    }
+};
---
+++ stellarUI/src/pages/lockedpage.cpp
@@ -0,0 +1,25 @@
+#include "LockedPage.h"
+#include "MainPage.h"  
+#include "../UIManager.h" 
+
+extern UIManager uiManager;
+extern RTCService rtcservice;
+extern FrameRateService framerateservice;
+
+LockedPage::LockedPage() {
+    auto* background = new StarryBackground(240, 280, 80);
+    addChild(background);
+    auto* frameText = new LiveText(200, 20, TFT_WHITE, []() {
+        return String(framerateservice.getFPS());
+    },2);
+    addChild(frameText);
+    auto* label = new Text(120, 260, "^", 3, TFT_WHITE, true);
+    addChild(label);
+    auto* gesture = new GestureDetector(120,140,240,280,30);
+    auto* clock = new ClockText(120, 130, 4, TFT_WHITE, true);
+    addChild(clock);
+    gesture->onSwipeUp = []() {
+        uiManager.pushPage(new MainPage(),TransitionType::SlideUp);
+    };
+    addChild(gesture);
+}
